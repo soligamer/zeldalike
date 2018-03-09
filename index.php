973d0158ade68bed024ee19f811c646ce702abc5
Kyhlanee <magali.swk@gmail.com>
2018-03-09 16:26:28 +0100

    MaJ

--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Hello World</title>
+  <title>ZeldaLike</title>
 </head>
   <script src="pixi/pixi.min.js"></script>
+  <script src="json/csvjson.json"></script>
 <body>
-  <script src="js/main.js"></script>
+ 
+    <script src2="js/main.js"></script>
+    <script src="js/script.js"></script>
+
 </body>
 </html>
